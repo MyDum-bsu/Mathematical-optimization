8bada2159661343a6a267f1b4bfc95efdc2b4662
MyDum-bsu <mydum.bsu@gmail.com>
2024-11-18 17:31:41 +0300

    Add code for GitHub Profile avatar

--- logo.py
+++ logo.py
@@ -1,23 +1,23 @@
 from manim import *
 import numpy as np
 
-a = 2
-b = 1
+a = 2.4
+b = 1.2
 h = np.sqrt(a ** 2 - b ** 2)
 #
 c = a + b
 
-num_lines = 120
+num_lines = 150
 
 num_radial_curves = 33
 max_curve_radius = a * 0.8
 
 angle_step = TAU / num_lines
 
-start_stroke = 4
+start_stroke = 7
 
-br_step = 1 / num_lines
-st_step = 2 / num_lines
+br_step = 1 / num_lines * 1.3
+st_step = start_stroke / num_lines
 
 SECTOR_COLOR = '#1f1c17'
 LINES_COLOR = '#9a5a18'
@@ -85,7 +85,7 @@ def draw_right_lines():
             end_point = np.array([end_x, end_y, 0])
             x, y = 10 * np.cos(alpha), 10 * np.sin(alpha)
             first_lines.add(
-                Line(end_point, np.array([x, y, 0]), color=LINES_COLOR, stroke_width=0.5, stroke_opacity=1))
+                Line(end_point, np.array([x, y, 0]), color=LINES_COLOR, stroke_width=0.7, stroke_opacity=0.5))
             first_lines.add(Line(start_point, end_point, color=GRAY, stroke_width=stroke, stroke_opacity=brightness))
 
         brightness -= br_step
@@ -144,7 +144,14 @@ class Logo(Scene):
         curves = get_center_curves()
         circle1 = Circle(radius=a).move_to(LEFT * b)
         circle2 = Circle(radius=a).move_to(RIGHT * b)
-        intersection_area = Intersection(circle1, circle2, fill_opacity=1).set_color(SECTOR_COLOR).scale(
-            0.9)
+        intersection_area = (
+            Intersection(circle1, circle2, fill_opacity=0.6).set_color(BLACK).set_stroke(color=CURVE_COLOR, width=2,
+                                                                                         opacity=0.35)
+            .scale(
+                0.93))
         self.add(intersection_area)
         self.add(curves.scale(0.5))
+        self.add(*[Circle(radius=i * a, color=BLACK) for i in np.arange(0.1, 0.357, 0.0225)])
+        self.add(Circle(radius=0.388 * a, color=BLACK))
+        # ManimColor('#000044')
+        self.add(Circle(radius=0.055 * a, color=DARKER_GRAY, fill_opacity=1))
